9ae79d4858fce10edbbda12a4ba11d2bba447c72
gitesh.more <giteshmore.pm@gmail.com>
2026-05-07 16:37:46 +0530

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -7,100 +7,98 @@ document.body.insertAdjacentHTML("beforeend", `
 
 <!-- Brand -->
 <div class="footer-section">
-<h3>Vedartham</h3>
-<p>
-Vedartham shares Vedic wisdom including Jyotirlinga temples,
-mantras, stotras, numerology, and spiritual guidance.
-</p>
+<h3>वेदार्थम्</h3>
+<h5>
+वेदार्थम् के माध्यम से मंत्र, स्तोत्र, ज्योतिर्लिंग, अंक ज्योतिष एवं आध्यात्मिक ज्ञान को सरल और प्रामाणिक रूप में प्रस्तुत किया जाता है।
+</h5>
 </div>
-
 <!-- Quick Links -->
 <div class="footer-section">
-<h3>Quick Links</h3>
+<h3>त्वरित लिंक</h3>
 <ul>
-<li><a href="index.html">Home</a></li>
-<li><a href="about.html">About Us</a></li>
-<li><a href="mailto:vedartham6422@gmail.com">Contact Us</a></li>
+<li><a href="index.html">होम</a></li>
+<li><a href="about.html">हमारे बारे में</a></li>
+<li><a href="mailto:vedartham6422@gmail.com">संपर्क करें</a></li>
 </ul>
 </div>
 
 <!-- Jyotirlinga -->
 <div class="footer-section">
-<h3>Jyotirlinga</h3>
+<h3>ज्योतिर्लिंग</h3>
 <ul>
-<li><a href="Jyotirlinga.html#somnath">Somnath</a></li>
-<li><a href="Jyotirlinga.html#kedarnath">Kedarnath</a></li>
-<li><a href="Jyotirlinga.html#mallikarjuna">Mallikarjuna</a></li>
-<li><a href="Jyotirlinga.html#mahakaleshwar">Mahakaleshwar</a></li>
-<li><a href="Jyotirlinga.html#omkareshwar">Omkareshwar</a></li>
-<li><a href="Jyotirlinga.html#bhimashankar">Bhimashankar</a></li>
+<li><a href="Jyotirlinga.html#somnath">सोमनाथ</a></li>
+<li><a href="Jyotirlinga.html#kedarnath">केदारनाथ</a></li>
+<li><a href="Jyotirlinga.html#mallikarjuna">मल्लिकार्जुन</a></li>
+<li><a href="Jyotirlinga.html#mahakaleshwar">महाकालेश्वर</a></li>
+<li><a href="Jyotirlinga.html#omkareshwar">ओंकारेश्वर</a></li>
+<li><a href="Jyotirlinga.html#bhimashankar">भीमाशंकर</a></li>
 </ul>
 </div>
 
 <!-- More Jyotirlinga -->
 <div class="footer-section">
-<h3>More Jyotirlinga</h3>
+<h3>अन्य ज्योतिर्लिंग</h3>
 <ul>
-<li><a href="Jyotirlinga.html#kashi">Kashi Vishwanath</a></li>
-<li><a href="Jyotirlinga.html#trimbakeshwar">Trimbakeshwar</a></li>
-<li><a href="Jyotirlinga.html#vaidyanath">Vaidyanath</a></li>
-<li><a href="Jyotirlinga.html#nageshwar">Nageshwar</a></li>
-<li><a href="Jyotirlinga.html#rameshwaram">Rameshwaram</a></li>
-<li><a href="Jyotirlinga.html#grishneshwar">Grishneshwar</a></li>
+<li><a href="Jyotirlinga.html#kashi">काशी विश्वनाथ</a></li>
+<li><a href="Jyotirlinga.html#trimbakeshwar">त्र्यंबकेश्वर</a></li>
+<li><a href="Jyotirlinga.html#vaidyanath">वैद्यनाथ</a></li>
+<li><a href="Jyotirlinga.html#nageshwar">नागेश्वर</a></li>
+<li><a href="Jyotirlinga.html#rameshwaram">रामेश्वरम</a></li>
+<li><a href="Jyotirlinga.html#grishneshwar">घृष्णेश्वर</a></li>
 </ul>
 </div>
 
 <!-- Rashi -->
 <div class="footer-section">
-<h3>Rashi Wise</h3>
+<h3>राशि अनुसार</h3>
 <ul>
-<li><a href="Rashi.html#Mesha">Mesha</a></li>
-<li><a href="Rashi.html#Vrishabha">Vrishabha</a></li>
-<li><a href="Rashi.html#Mithun">Mithun</a></li>
-<li><a href="Rashi.html#Karka">Karka</a></li>
-<li><a href="Rashi.html#Simha">Simha</a></li>
-<li><a href="Rashi.html#Kanya">Kanya</a></li>
+<li><a href="Rashi.html#Mesha">मेष</a></li>
+<li><a href="Rashi.html#Vrishabha">वृषभ</a></li>
+<li><a href="Rashi.html#Mithun">मिथुन</a></li>
+<li><a href="Rashi.html#Karka">कर्क</a></li>
+<li><a href="Rashi.html#Simha">सिंह</a></li>
+<li><a href="Rashi.html#Kanya">कन्या</a></li>
 </ul>
 </div>
 
 <!-- More Rashi -->
 <div class="footer-section">
-<h3>More Rashi</h3>
+<h3>अन्य राशियाँ</h3>
 <ul>
-<li><a href="Rashi.html#Tula">Tula</a></li>
-<li><a href="Rashi.html#Vrischika">Vrischika</a></li>
-<li><a href="Rashi.html#Dhanu">Dhanu</a></li>
-<li><a href="Rashi.html#Makara">Makara</a></li>
-<li><a href="Rashi.html#Kumbha">Kumbha</a></li>
-<li><a href="Rashi.html#Meena">Meena</a></li>
+<li><a href="Rashi.html#Tula">तुला</a></li>
+<li><a href="Rashi.html#Vrischika">वृश्चिक</a></li>
+<li><a href="Rashi.html#Dhanu">धनु</a></li>
+<li><a href="Rashi.html#Makara">मकर</a></li>
+<li><a href="Rashi.html#Kumbha">कुंभ</a></li>
+<li><a href="Rashi.html#Meena">मीन</a></li>
 </ul>
 </div>
 
 <!-- Numerology -->
 <div class="footer-section">
-<h3>Numerology</h3>
+<h3>अंक ज्योतिष</h3>
 <ul>
-<li><a href="Numerology.html#num1">Number 1</a></li>
-<li><a href="Numerology.html#num2">Number 2</a></li>
-<li><a href="Numerology.html#num3">Number 3</a></li>
-<li><a href="Numerology.html#num4">Number 4</a></li>
-<li><a href="Numerology.html#num5">Number 5</a></li>
-<li><a href="Numerology.html#num6">Number 6</a></li>
-<li><a href="Numerology.html#num7">Number 7</a></li>
-<li><a href="Numerology.html#num8">Number 8</a></li>
-<li><a href="Numerology.html#num9">Number 9</a></li>
+<li><a href="Numerology.html#num1">अंक 1</a></li>
+<li><a href="Numerology.html#num2">अंक 2</a></li>
+<li><a href="Numerology.html#num3">अंक 3</a></li>
+<li><a href="Numerology.html#num4">अंक 4</a></li>
+<li><a href="Numerology.html#num5">अंक 5</a></li>
+<li><a href="Numerology.html#num6">अंक 6</a></li>
+<li><a href="Numerology.html#num7">अंक 7</a></li>
+<li><a href="Numerology.html#num8">अंक 8</a></li>
+<li><a href="Numerology.html#num9">अंक 9</a></li>
 </ul>
 </div>
 
 <!-- Other Sections -->
 <div class="footer-section">
-<h3>Explore</h3>
+<h3>अन्वेषण करें</h3>
 <ul>
-<li><a href="DayWise.html#daywise">Day Wise Mantra</a></li>
-<li><a href="Occasions.html#occasions">Occasions</a></li>
-<li><a href="Stotra.html#stotra">Stotra</a></li>
-<li><a href="Festivals.html#festivals">Festivals</a></li>
-<li><a href="Temples.html#temples">Temples</a></li>
+<li><a href="DayWise.html#daywise">दिन अनुसार मंत्र</a></li>
+<li><a href="Occasions.html#occasions">विशेष अवसर</a></li>
+<li><a href="Stotra.html#stotra">स्तोत्र</a></li>
+<li><a href="Festivals.html#festivals">त्योहार</a></li>
+<li><a href="Temples.html#temples">मंदिर</a></li>
 </ul>
 </div>
 
